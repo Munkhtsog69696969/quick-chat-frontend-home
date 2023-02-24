242680232f460ad1cb13b6befb699986d95c89a7
heisenberg <sda@example.com>
2023-02-24 21:00:56 +0800

    sda

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/' element={<Signup/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
         <Route path='/createroom' element={<CreateRoom/>}/>
